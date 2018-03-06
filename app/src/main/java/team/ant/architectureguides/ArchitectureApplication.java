744687460648b1c1ee3ad3200e048485722d5434
NachoVazquez <nachovc1410@gmail.com>
2018-03-05 19:06:39 -0500

    Creates custom Application

---
+++ app/src/main/java/team/ant/architectureguides/ArchitectureApplication.java
@@ -0,0 +1,15 @@
+package team.ant.architectureguides;
+
+import android.app.Application;
+
+/**
+ * Created by Nacho Vazquez on 3/5/2018.
+ */
+
+public class ArchitectureApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
