fad171ba30ad59aaaaaa42c940ccc152b28f9ea2
NachoVazquez <nachovc1410@gmail.com>
2018-03-05 19:02:24 -0500

    Creates application qualifier

---
+++ app/src/main/java/team/ant/architectureguides/di/ForApplication.java
@@ -0,0 +1,14 @@
+package team.ant.architectureguides.di;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Qualifier;
+
+/**
+ * Created by Nacho Vazquez on 3/5/2018.
+ */
+@Qualifier
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ForApplication {
+}
