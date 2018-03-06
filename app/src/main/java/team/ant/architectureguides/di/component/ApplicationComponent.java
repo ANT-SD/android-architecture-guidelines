8d9b0122547a5ab11e0ff0efe5a3d1a08e8d017c
NachoVazquez <nachovc1410@gmail.com>
2018-03-06 17:14:59 -0500

    Provides dependencies for Dependent Components
    * Also specifies inject for ArchitectureApplication

--- app/src/main/java/team/ant/architectureguides/di/component/ApplicationComponent.java
+++ app/src/main/java/team/ant/architectureguides/di/component/ApplicationComponent.java
@@ -1,7 +1,15 @@
 package team.ant.architectureguides.di.component;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+
+import com.google.gson.Gson;
+
 import dagger.Component;
+import retrofit2.Retrofit;
+import team.ant.architectureguides.ArchitectureApplication;
 import team.ant.architectureguides.di.ApplicationScope;
+import team.ant.architectureguides.di.ForApplication;
 import team.ant.architectureguides.di.module.AndroidModule;
 import team.ant.architectureguides.di.module.ApplicationModule;
 import team.ant.architectureguides.di.module.GsonModule;
@@ -19,4 +27,25 @@ import team.ant.architectureguides.di.module.OkHttpInterceptorsModule;
         OkHttpInterceptorsModule.class,
         GsonModule.class})
 public interface ApplicationComponent {
+
+    @ForApplication
+    Context provideContext();
+
+    LayoutInflater provideLayoutInflater();
+    Retrofit provideRetrofit();
+    Gson provideGson();
+
+    void inject (ArchitectureApplication application);
+
+//    final class Initializer {
+//        public static ApplicationComponent init(ArchitectureApplication application) {
+//            return DaggerApplicationComponent.builder()
+//                    .androidModule(new AndroidModule())
+//                    .applicationModule(new ApplicationModule(application))
+//                    .gsonModule(new GsonModule())
+//                    .networkModule(new NetworkModule(application))
+//                    .okHttpInterceptorsModule(new OkHttpInterceptorsModule())
+//                    .build();
+//        }
+//    }
 }
