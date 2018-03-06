082eecbea01e98205acb45e9061833d5e7045787
NachoVazquez <nachovc1410@gmail.com>
2018-03-06 13:42:55 -0500

    Adds OkHttpInterceptorsModule into ApplicationComponent

--- app/src/main/java/team/ant/architectureguides/di/component/ApplicationComponent.java
+++ app/src/main/java/team/ant/architectureguides/di/component/ApplicationComponent.java
@@ -5,11 +5,16 @@ import team.ant.architectureguides.di.ApplicationScope;
 import team.ant.architectureguides.di.module.AndroidModule;
 import team.ant.architectureguides.di.module.ApplicationModule;
 import team.ant.architectureguides.di.module.NetworkModule;
+import team.ant.architectureguides.di.module.OkHttpInterceptorsModule;
 
 /**
  * Created by Nacho Vazquez on 3/6/2018.
  */
 @ApplicationScope
-@Component(modules = {AndroidModule.class, ApplicationModule.class, NetworkModule.class})
+@Component(modules = {
+        AndroidModule.class,
+        ApplicationModule.class,
+        NetworkModule.class,
+        OkHttpInterceptorsModule.class})
 public interface ApplicationComponent {
 }
