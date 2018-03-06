2b9fe4d3cabba2bb95dea7289be180cbf9cd7882
NachoVazquez <nachovc1410@gmail.com>
2018-03-05 19:04:15 -0500

    Creates application singleton scope

---
+++ app/src/main/java/team/ant/architectureguides/di/ApplicationScope.java
@@ -0,0 +1,14 @@
+package team.ant.architectureguides.di;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Scope;
+
+/**
+ * Created by Nacho Vazquez on 3/5/2018.
+ */
+@Scope
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ApplicationScope {
+}
