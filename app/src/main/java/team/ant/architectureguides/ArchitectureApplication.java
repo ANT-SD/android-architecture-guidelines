9cb52ab5e2d9ea60672987e098c242415c9ffb70
NachoVazquez <nachovc1410@gmail.com>
2018-03-06 17:46:37 -0500

    Initializes ApplicationComponent and provides via get the Application

--- app/src/main/java/team/ant/architectureguides/ArchitectureApplication.java
+++ app/src/main/java/team/ant/architectureguides/ArchitectureApplication.java
@@ -1,6 +1,12 @@
 package team.ant.architectureguides;
 
 import android.app.Application;
+import android.content.Context;
+
+import com.facebook.stetho.Stetho;
+
+import team.ant.architectureguides.di.component.ApplicationComponent;
+import timber.log.Timber;
 
 /**
  * Created by Nacho Vazquez on 3/5/2018.
@@ -8,8 +14,33 @@ import android.app.Application;
 
 public class ArchitectureApplication extends Application {
 
+    private ApplicationComponent applicationComponent;
+
     @Override
     public void onCreate() {
         super.onCreate();
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        }
+
+        applicationComponent = createComponent();
+        Stetho.initializeWithDefaults(this);
+    }
+
+    private ApplicationComponent createComponent() {
+        applicationComponent = ApplicationComponent.Initializer.init(this);
+        applicationComponent.inject(this);
+        return applicationComponent;
+    }
+
+    public ApplicationComponent getComponent() {
+        if (applicationComponent == null) {
+            createComponent();
+        }
+        return applicationComponent;
+    }
+
+    public static ArchitectureApplication get(Context context) {
+        return (ArchitectureApplication) context.getApplicationContext();
     }
 }
