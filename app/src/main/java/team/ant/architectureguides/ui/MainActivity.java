c2256dfbd65597de2bce18b27dfc5a08c88f659c
NachoVazquez <nachovc1410@gmail.com>
2018-03-05 19:09:01 -0500

    Moves MainActivity to ui folder

--- app/src/main/java/team/ant/architectureguides/ui/MainActivity.java
+++ app/src/main/java/team/ant/architectureguides/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package team.ant.architectureguides;
+package team.ant.architectureguides.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import team.ant.architectureguides.R;
 
 public class MainActivity extends AppCompatActivity {
 
