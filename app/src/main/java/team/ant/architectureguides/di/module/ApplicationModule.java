4cf39236b59d1adacd5ff01de22562523c6ff770
NachoVazquez <nachovc1410@gmail.com>
2018-03-07 13:07:44 -0500

    Cleans ApplicationModule

--- app/src/main/java/team/ant/architectureguides/di/module/ApplicationModule.java
+++ app/src/main/java/team/ant/architectureguides/di/module/ApplicationModule.java
@@ -5,40 +5,21 @@ import android.content.Context;
 
 import dagger.Module;
 import dagger.Provides;
-import team.ant.architectureguides.ArchitectureApplication;
-import team.ant.architectureguides.di.ActivityScope;
-import team.ant.architectureguides.di.ApplicationScope;
-import team.ant.architectureguides.di.ForApplication;
+import team.ant.architectureguides.di.qualifier.ForApplication;
+import team.ant.architectureguides.di.scope.ApplicationScope;
 
 /**
  * Created by Nacho Vazquez on 3/5/2018.
  */
 @Module
 public class ApplicationModule {
-    private ArchitectureApplication application;
-
-    public ApplicationModule(ArchitectureApplication application) {
-        this.application = application;
-    }
-
-    @ApplicationScope
-    @ForApplication
-    @Provides
-    public Application provideApplication(){
-        return this.application;
-    }
-
-    @ApplicationScope
-    @ForApplication
-    @Provides
-    public ArchitectureApplication provideArchitectureApplication(){
-        return this.application;
-    }
 
     @ForApplication
     @ApplicationScope
     @Provides
-    public Context provideContext() {
-        return this.application.getApplicationContext();
+    public Context provideContext(Application application) {
+        return application.getApplicationContext();
     }
+
+    //may also share sharedPref
 }
