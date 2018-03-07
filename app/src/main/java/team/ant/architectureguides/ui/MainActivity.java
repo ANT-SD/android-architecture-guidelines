cc3165df6270cb77b5154ad1d9e5fd9d8703f513
NachoVazquez <nachovc1410@gmail.com>
2018-03-07 12:58:56 -0500

    Adds DaggerAndroid Injection to MainActivity

--- app/src/main/java/team/ant/architectureguides/ui/MainActivity.java
+++ app/src/main/java/team/ant/architectureguides/ui/MainActivity.java
@@ -1,15 +1,28 @@
 package team.ant.architectureguides.ui;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import javax.inject.Inject;
+
+import dagger.android.AndroidInjector;
+import dagger.android.DispatchingAndroidInjector;
+import dagger.android.support.HasSupportFragmentInjector;
 import team.ant.architectureguides.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements HasSupportFragmentInjector {
+    @Inject
+    DispatchingAndroidInjector<Fragment> dispatchingAndroidInjector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public AndroidInjector<Fragment> supportFragmentInjector() {
+        return dispatchingAndroidInjector;
     }
 }
