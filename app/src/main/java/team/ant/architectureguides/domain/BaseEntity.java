efff3bc2c1d1726a49d723d0b5ae2bfe2c5fb2d1
NachoVazquez <nachovc1410@gmail.com>
2018-03-07 13:37:11 -0500

    Makes BaseEntity generic on key Type

--- app/src/main/java/team/ant/architectureguides/domain/BaseEntity.java
+++ app/src/main/java/team/ant/architectureguides/domain/BaseEntity.java
@@ -1,8 +1,15 @@
 package team.ant.architectureguides.domain;
 
+import android.arch.persistence.room.PrimaryKey;
+
 /**
  * Created by Nacho Vazquez on 3/7/2018.
  */
 
-public abstract class BaseEntity {
+public abstract class BaseEntity<T> {
+    @PrimaryKey
+    private T id;
+
+
+
 }
