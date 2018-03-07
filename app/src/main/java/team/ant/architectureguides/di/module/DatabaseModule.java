6cb84aa21d8cb65dc38ec0b46e5dc74d2ffe5fd9
NachoVazquez <nachovc1410@gmail.com>
2018-03-07 12:54:05 -0500

    Creates DatabaseModule (dagger)

---
+++ app/src/main/java/team/ant/architectureguides/di/module/DatabaseModule.java
@@ -0,0 +1,29 @@
+package team.ant.architectureguides.di.module;
+
+import dagger.Module;
+
+/**
+ * Created by Nacho Vazquez on 3/7/2018.
+ */
+@Module
+public abstract class DatabaseModule {
+
+
+//    @ApplicationScope
+//    @Provides
+//    GithubDb provideDb(Application app) {
+//        return Room.databaseBuilder(app, GithubDb.class,"github.db").build();
+//    }
+//
+//    @ApplicationScope
+//    @Provides
+//    UserDao provideUserDao(GithubDb db) {
+//        return db.userDao();
+//    }
+//
+//    @ApplicationScope
+//    @Provides
+//    RepoDao provideRepoDao(GithubDb db) {
+//        return db.repoDao();
+//    }
+}
