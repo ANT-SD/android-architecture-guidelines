12730c1f203d1591d8cdcf7aeee8e19b22402870
NachoVazquez <nachovc1410@gmail.com>
2018-03-07 13:01:17 -0500

    Adds @Component.Builder to ApplicationComponent with @BindsInstance

--- app/src/main/java/team/ant/architectureguides/di/component/ApplicationComponent.java
+++ app/src/main/java/team/ant/architectureguides/di/component/ApplicationComponent.java
@@ -1,20 +1,18 @@
 package team.ant.architectureguides.di.component;
 
-import android.content.Context;
-import android.view.LayoutInflater;
+import android.app.Application;
 
-import com.google.gson.Gson;
-
+import dagger.BindsInstance;
 import dagger.Component;
-import retrofit2.Retrofit;
+import dagger.android.AndroidInjectionModule;
 import team.ant.architectureguides.ArchitectureApplication;
-import team.ant.architectureguides.di.ApplicationScope;
-import team.ant.architectureguides.di.ForApplication;
 import team.ant.architectureguides.di.module.AndroidModule;
 import team.ant.architectureguides.di.module.ApplicationModule;
+import team.ant.architectureguides.di.module.DatabaseModule;
 import team.ant.architectureguides.di.module.GsonModule;
 import team.ant.architectureguides.di.module.NetworkModule;
 import team.ant.architectureguides.di.module.OkHttpInterceptorsModule;
+import team.ant.architectureguides.di.scope.ApplicationScope;
 
 /**
  * Created by Nacho Vazquez on 3/6/2018.
@@ -25,27 +23,20 @@ import team.ant.architectureguides.di.module.OkHttpInterceptorsModule;
         ApplicationModule.class,
         NetworkModule.class,
         OkHttpInterceptorsModule.class,
-        GsonModule.class})
+        GsonModule.class,
+        DatabaseModule.class,
+        AndroidInjectionModule.class})
 public interface ApplicationComponent {
 
-    @ForApplication
-    Context provideContext();
+    @Component.Builder
+    interface Builder {
+        @BindsInstance
+        Builder application(Application application);
 
-    LayoutInflater provideLayoutInflater();
-    Retrofit provideRetrofit();
-    Gson provideGson();
-
-    void inject (ArchitectureApplication application);
-
-    final class Initializer {
-        public static ApplicationComponent init(ArchitectureApplication application) {
-            return DaggerApplicationComponent.builder()
-                    .androidModule(new AndroidModule())
-                    .applicationModule(new ApplicationModule(application))
-                    .gsonModule(new GsonModule())
-                    .networkModule(new NetworkModule(application))
-                    .okHttpInterceptorsModule(new OkHttpInterceptorsModule())
-                    .build();
-        }
+        ApplicationComponent build();
     }
+
+    void inject(ArchitectureApplication application);
+
+
 }
